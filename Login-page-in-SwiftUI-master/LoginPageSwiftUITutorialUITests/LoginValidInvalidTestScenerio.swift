1a4302ce14b422586f1ffb396cbc81bb476bb560
Kavita Mishra <kmishra@keywordsstudios.com>
2021-04-22 18:07:16 +0530

    test1

---
+++ Login-page-in-SwiftUI-master/LoginPageSwiftUITutorialUITests/LoginValidInvalidTestScenerio.swift
@@ -0,0 +1,29 @@
+//
+//  LoginValidInvalidTestScenerio.swift
+//  LoginPageSwiftUITutorialUITests
+//
+//  Created by Keywords Studio on 08/04/21.
+//  Copyright © 2021 Andreas Schultz. All rights reserved.
+//
+
+import XCTest
+
+class LoginValidInvalidTestScenerio: XCTestCase {
+
+    override func setUpWithError() throws {
+      
+        continueAfterFailure = false
+
+    
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+
+    }
+
+}
